a024dd09a7eb268790f12c3ea67b1effd97e52f4
Kydoimos97 <willemvanderschans97@gmail.com>
2025-11-02 21:37:54 -0700

    Add literal type definitions to `xpytools.types.literal` module

---
+++ xpytools/types/literal/__init__.py
@@ -0,0 +1,11 @@
+from .StrLiteral import StrLiteral
+from .FloatLiteral import FloatLiteral
+from .IntLiteral import IntLiteral
+from .AnyTLiteral import AnyTLiteral
+
+__all__ = [
+        'StrLiteral',
+        'FloatLiteral',
+        'IntLiteral',
+        'AnyTLiteral'
+        ]
